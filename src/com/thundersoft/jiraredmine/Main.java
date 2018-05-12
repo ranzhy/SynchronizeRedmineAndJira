61dadcbde01d83d7f7f1a8f657397d5c42f96920
Ran Zhengyuan <ranzy0631@thundersoft.com>
2018-05-12 09:43:28 +0800

    Log the exception which will make the process end

--- src/com/thundersoft/jiraredmine/Main.java
+++ src/com/thundersoft/jiraredmine/Main.java
@@ -1,9 +1,5 @@
 package com.thundersoft.jiraredmine;
 
-import java.net.URL;
-import java.net.URLStreamHandler;
-import java.net.URLStreamHandlerFactory;
-
 import com.taskadapter.redmineapi.RedmineException;
 import com.taskadapter.redmineapi.RedmineManager;
 import com.taskadapter.redmineapi.RedmineManagerFactory;
@@ -22,52 +18,47 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty ("jsse.enableSNIExtension", "false");
 
-//        URL.setURLStreamHandlerFactory(new URLStreamHandlerFactory() {
-//
-//            @Override
-//            public URLStreamHandler createURLStreamHandler(String protocol) {
-//                if ("file".equals(protocol)) {
-//                    return new sun.net.www.protocol.file.Handler();
-//                }
-//                return null;
-//            }
-//            
-//        });
         Log.info(Main.class, "================= Issue Syncronize =====================");
 
-        SystemConfig system = SystemConfig.getInstance();
-        ServerConfig config = system.getServerConfig();
+        try {
+            SystemConfig system = SystemConfig.getInstance();
+            ServerConfig config = system.getServerConfig();
 
-        RedmineManager redmine = RedmineManagerFactory.createWithUserAuth(
-                config.getRedmineUrl(), config.getRedmineLoginUser(), config.getRedmineLoginPasswd());
+            RedmineManager redmine = RedmineManagerFactory.createWithUserAuth(
+                    config.getRedmineUrl(), config.getRedmineLoginUser(), config.getRedmineLoginPasswd());
 
-        Log.info(Main.class, "Will create Issue handler for " + redmine);
-        AbstractIssueHandler issueHandler = system.createIssueHandler(redmine);
-        Log.info(Main.class, "Created Issue handler: " + issueHandler);
+            Log.info(Main.class, "Will create Issue handler for " + redmine);
+            AbstractIssueHandler issueHandler = system.createIssueHandler(redmine);
+            Log.info(Main.class, "Created Issue handler: " + issueHandler);
 
-        RedmineSynchronizer synchronizer = new RedmineSynchronizer(redmine.getIssueManager(), issueHandler);
-        LocalIssueManager issugMgr = new LocalIssueManager();
+            RedmineSynchronizer synchronizer = new RedmineSynchronizer(redmine.getIssueManager(), issueHandler);
+            LocalIssueManager issugMgr = new LocalIssueManager();
 
-        Log.info(Main.class, "Will load and synchronize redmine issues and jira issues");
-        issugMgr.loadAllRedmineIssue(redmine.getIssueManager());
-        issugMgr.loadAllJiraIssue();
-        issugMgr.compareIssues(new IssueCompareCallback() {
+            Log.info(Main.class, "Will load and synchronize redmine issues and jira issues");
+            issugMgr.loadAllRedmineIssue(redmine.getIssueManager());
+            issugMgr.loadAllJiraIssue();
+            issugMgr.compareIssues(new IssueCompareCallback() {
 
-            @Override
-            public void onCompare(Issue redmine, JiraIssue jira, boolean clearRedmine) {
-                try {
-                    synchronizer.synchronize(redmine, jira, clearRedmine);
-                } catch (RedmineException e) {
-                    Log.error(getClass(),
-                            "Synchronizing jira [" + jira + "] and redmine [" + redmine + "]", e);
+                @Override
+                public void onCompare(Issue redmine, JiraIssue jira, boolean clearRedmine) {
+                    try {
+                        synchronizer.synchronize(redmine, jira, clearRedmine);
+                    } catch (RedmineException e) {
+                        Log.error(getClass(),
+                                "Synchronizing jira [" + jira + "] and redmine [" + redmine + "]", e);
+                    }
                 }
-            }
 
-            @Override
-            public void onCompareCompleted() {
-            }
-        });
-        Log.info(Main.class, "=========== Issue Syncronize completed =====================");
+                @Override
+                public void onCompareCompleted() {
+                    Log.info(Main.class, "Issue Compare completed");
+                }
+            });
+        } catch (Exception e) {
+            Log.error(Main.class, "Syncronizing failed between JIRA and redmine in this time", e);
+        } finally {
+            Log.info(Main.class, "=========== Issue Syncronize completed =====================");
+        }
     }
 
 }
